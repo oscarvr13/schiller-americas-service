50dd516da2177356084b10edee1960d9ba980157
Oscar Vazquez <oscarvr1213@gmail.com>
2020-06-19 10:46:33 -0500

    Adding integration tests

---
+++ src/test/resources/data.sql
@@ -0,0 +1,5 @@
+insert into Person(id, name, age, gender, code) values ('1', 'Juan', 24, 'M', 'abcdefghij')
+insert into Person(id, name, age, gender, code) values ('2', 'Maria', 36, 'F', 'mnopqrstuvq')
+insert into Person(id, name, age, gender, code) values ('3', 'Luis', 17, 'M', 'qwertymnbvc')
+
+
