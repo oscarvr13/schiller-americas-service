7f58c1e211badef3c10716bdec98f2d09b56e270
Oscar Vazquez <oscarvr1213@gmail.com>
2020-06-29 18:44:57 -0500

    Adding Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,7 +9,7 @@ pipeline {
         }
         stage('Build') {
             steps {
-                echo 'Building Sample Maven Project'git
+                echo 'Building Sample Maven Project'
             }
         }
         stage('Deploy') {
